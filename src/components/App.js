765c4e13914f766ec3dd01d6c5648967a335bdae
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-10-27 12:38:16 -0500

    merged codes front end movieDisplay with watchlistDisplay

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,31 @@ import './App.css';
 import Watchlist from './Watchlist'
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+import Row from './Row';
+import requests from './Requests';
+import Banner from './Banner';
 
-const App = () => {
 
-return (
-  <div className='App'>
-      <h1>Navbar Place Holder</h1>
-      <Switch>
+function App({ props }) {
+	return (
+	<div className="App">
+		<h1>Navbar Place Holder</h1>
+    <Banner />
+    <Switch>
       <Route
 					path='/watchlist'
 					render={(routerProps) => <Watchlist {...routerProps} user='Gore' />}
           />
       </Switch>
-    </div>
-  );
+		<Row title="CINEPRIME ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+		<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+		<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+		<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+		<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+		<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+		<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+	</div>
+	);
 }
 
 export default App;
