cffb469d791d8e08a2e085a68f84734a5a196378
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-10-27 11:51:06 -0500

    feature movieDisplay in scrollable rows

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './Requests';
+import Banner from './Banner';
 
-function App() {
-	return <h1>The App Component</h1>;
+function App({ props }) {
+	return (
+	<div className="App">
+		<Banner />
+		<Row title="CINEPRIME ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+		<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+		<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+		<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+		<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+		<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+		<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+	</div>
+	);
 }
 
 export default App;
