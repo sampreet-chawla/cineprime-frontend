83aff67003d7cbba0ee4651d80c22f42bc04ac16
josilob <bjosilo@gmail.com>
2020-10-27 21:13:06 -0400

    nav and footer done, teampage in progress

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,8 +1,78 @@
-import React from "react";
-import './Team.css';
+import React from 'react';
+// import './Team.css';
 
-function Team ({ props }) {
-    return(<h3>Team Component</h3>)
+function Team() {
+	return (
+		<div class='team'>
+			<div class='container'>
+				<h1>Team that worked on this project:</h1>
+				<div class='card'>
+					<div class='box'>
+						<img src='sampreet3.jpg' alt='img' />
+						<h4>Sampreet Chawla</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+							pellentesque at urna et vestibulum. Donec egestas nulla non dolor
+							imperdiet maximus. Sed eget egestas orci. In id justo auctor,
+							euismod justo ac, cursus enim. Nam in viverra massa, a vulputate
+							nisi. Etiam finibus enim sit amet fringilla consectetur. Nullam
+							purus augue, molestie vitae eleifend viverra, pellentesque a
+							sapien. Aliquam a augue dignissim mauris sodales ultricies quis
+							sit amet felis. Suspendisse et sodales nibh.
+						</p>
+					</div>
+				</div>
+				<div class='card'>
+					<div class='box'>
+						<img src='shayan.jpg' alt='img' />
+						<h4>Gagan Shayan</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+							pellentesque at urna et vestibulum. Donec egestas nulla non dolor
+							imperdiet maximus. Sed eget egestas orci. In id justo auctor,
+							euismod justo ac, cursus enim. Nam in viverra massa, a vulputate
+							nisi. Etiam finibus enim sit amet fringilla consectetur. Nullam
+							purus augue, molestie vitae eleifend viverra, pellentesque a
+							sapien. Aliquam a augue dignissim mauris sodales ultricies quis
+							sit amet felis. Suspendisse et sodales nibh.
+						</p>
+					</div>
+				</div>
+				<div class='card'>
+					<div class='box'>
+						<img src='bojan.jpg' alt='img' id='bojan' />
+						<h4>Bojan Josilo</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+							pellentesque at urna et vestibulum. Donec egestas nulla non dolor
+							imperdiet maximus. Sed eget egestas orci. In id justo auctor,
+							euismod justo ac, cursus enim. Nam in viverra massa, a vulputate
+							nisi. Etiam finibus enim sit amet fringilla consectetur. Nullam
+							purus augue, molestie vitae eleifend viverra, pellentesque a
+							sapien. Aliquam a augue dignissim mauris sodales ultricies quis
+							sit amet felis. Suspendisse et sodales nibh.
+						</p>
+					</div>
+				</div>
+				<div class='card'>
+					<div class='box'>
+						<img src='gore.jpg' alt='img' />
+						<h4>Gourav Auluck</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+							pellentesque at urna et vestibulum. Donec egestas nulla non dolor
+							imperdiet maximus. Sed eget egestas orci. In id justo auctor,
+							euismod justo ac, cursus enim. Nam in viverra massa, a vulputate
+							nisi. Etiam finibus enim sit amet fringilla consectetur. Nullam
+							purus augue, molestie vitae eleifend viverra, pellentesque a
+							sapien. Aliquam a augue dignissim mauris sodales ultricies quis
+							sit amet felis. Suspendisse et sodales nibh.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Team;
